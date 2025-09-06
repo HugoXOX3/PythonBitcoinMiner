cf7ac1f2c7acbe095f37721b27589496f4952219
shebasatan <satansheba@gmail.com>
2025-09-06 09:08:46 -0700

    Create Escape4p.bat

---
+++ Escape4p.bat
@@ -0,0 +1,94 @@
+≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+≈≈≈≈≈≈
+                   Welcome to termux              
+                         htmlgold                 
+                                                  
+              Sat Sep  6 08:45:07 PDT 2025        
+                                                  
+                      Good morning                
+
+≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+≈≈≈≈≈≈
+~ >>gem install fractal
+Fetching fractal-0.2.0.gem
+Fetching chunky_png-1.4.0.gem
+Successfully installed chunky_png-1.4.0
+Successfully installed fractal-0.2.0
+Parsing documentation for chunky_png-1.4.0
+Installing ri documentation for chunky_png-1.4.0
+Parsing documentation for fractal-0.2.0
+Installing ri documentation for fractal-0.2.0
+Done installing documentation for chunky_png, frac
+tal after 8 seconds
+2 gems installed
+~ >>gem install rb sh nefly pixel
+Fetching rb-0.0.1.gem
+Successfully installed rb-0.0.1
+Parsing documentation for rb-0.0.1
+Installing ri documentation for rb-0.0.1
+Done installing documentation for rb after 1 secon
+ds
+Fetching sh-0.0.2.gem
+Fetching escape-0.0.4.gem
+Successfully installed escape-0.0.4
+Successfully installed sh-0.0.2
+Parsing documentation for escape-0.0.4
+Installing ri documentation for escape-0.0.4
+Parsing documentation for sh-0.0.2
+Installing ri documentation for sh-0.0.2
+Done installing documentation for escape, sh after
+ 0 seconds
+ERROR:  Could not find a valid gem 'nefly' (>= 0) 
+in any repository
+ERROR:  Possible alternatives: defly, nelly, newly
+, nofly
+Fetching pixel-0.2.0.gem
+Successfully installed pixel-0.2.0
+Parsing documentation for pixel-0.2.0
+Installing ri documentation for pixel-0.2.0
+Done installing documentation for pixel after 0 se
+conds
+4 gems installed
+~ >>e5
+No command e5 found, did you mean:
+ Command ed in package ed
+ Command ef in package electric-fence
+ Command et in package et
+ Command ex in package vim-gtk from the x11-repo r
+epository
+~ >>gem uninstall
+ERROR:  While executing gem ... (Gem::CommandLineE
+rror)
+    Please specify at least one gem name (Gem::Com
+mandLineError)
+        /data/data/com.termux/files/usr/lib/ruby/3
+.4.0/rubygems/command.rb:189:in 'Gem::Command#get_
+all_gem_names'
+        /data/data/com.termux/files/usr/lib/ruby/3
+.4.0/rubygems/command.rb:202:in 'Gem::Command#get_
+all_gem_names_and_versions'
+        /data/data/com.termux/files/usr/lib/ruby/3
+.4.0/rubygems/commands/uninstall_command.rb:155:in
+ 'Gem::Commands::UninstallCommand#uninstall_specif
+ic'
+        /data/data/com.termux/files/usr/lib/ruby/3
+.4.0/rubygems/commands/uninstall_command.rb:136:in
+ 'Gem::Commands::UninstallCommand#execute'
+        /data/data/com.termux/files/usr/lib/ruby/3
+.4.0/rubygems/command.rb:326:in 'Gem::Command#invo
+ke_with_build_args'
+        /data/data/com.termux/files/usr/lib/ruby/3
+.4.0/rubygems/command_manager.rb:253:in 'Gem::Comm
+andManager#invoke_command'
+        /data/data/com.termux/files/usr/lib/ruby/3
+.4.0/rubygems/command_manager.rb:194:in 'Gem::Comm
+andManager#process_args'
+        /data/data/com.termux/files/usr/lib/ruby/3
+.4.0/rubygems/command_manager.rb:152:in 'Gem::Comm
+andManager#run'
+        /data/data/com.termux/files/usr/lib/ruby/3
+.4.0/rubygems/gem_runner.rb:57:in 'Gem::GemRunner#
+run'
+        /data/data/com.termux/files/usr/bin/gem:12
+:in '<main>'
+~ >>
